40796f2e28a31233a33eee11b972b34311e7fc00
Lenny_Hsu <Lenny_Hsu@compal.com>
2017-05-12 19:11:11 +0800

    加入專案檔案。

---
+++ MVC5_HomeWork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_HomeWork.Startup))]
+namespace MVC5_HomeWork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
